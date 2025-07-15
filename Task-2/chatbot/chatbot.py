b47f22f1e0fdb8a6e19aee15c91f9097241ee42f
Karthik0000007 <sairamkarthik2005@gmail.com>
2025-07-15 11:18:49 +0530

    Day 15: ntegrated sentiment detection into chatbot for emotion-aware response prompts

--- Task-2/chatbot/chatbot.py
+++ Task-2/chatbot/chatbot.py
@@ -1,7 +1,11 @@
-import sys
 import os
+import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from embeddings.embedder import embed_chunks, model
+task3_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Task-3'))
+if task3_path not in sys.path:
+    sys.path.append(task3_path)
+from embeddings.embedder import model
+from sentiment.analyzer import analyze_sentiment
 from vector_store.fiass_db import FAISSVectorStore
 import subprocess
 
@@ -15,23 +19,37 @@ def query_ollama(prompt, model_name="llama3"):
     return result.stdout.decode("utf-8").strip()
 
 def generate_rag_response(user_query, top_k=3):
-    query_embedding = model.encode([user_query])[0]
 
+    sentiment = analyze_sentiment(user_query)
+    query_embedding = model.encode([user_query])[0]
     store = FAISSVectorStore()
     top_chunks = store.search(query_embedding, top_k=top_k)
-
     context = "\n\n".join(top_chunks)
-    prompt = f"""You are a helpful assistant. Use the following context to answer the user's question. 
-    {context}
 
-    {user_query}
+    tone = {
+        "positive": "Maintain a cheerful, thankful tone.",
+        "neutral": "Be clear, professional, and helpful.",
+        "negative": "Be empathetic, apologize briefly, and offer clear solutions."
+    }[sentiment]
 
-    Answer clearly:
+    prompt = f"""
+You are a helpful customer service chatbot.
+
+Use the following knowledge to answer the user question.
+Tone guide: {tone}
+
+ CONTEXT 
+{context}
+
+ USER QUERY 
+{user_query}
+
+Answer appropriately:
 """
     return query_ollama(prompt)
 
-# Testing 
+#Testing the RAG response generation
 if __name__ == "__main__":
-    user_input = "How do chatbots use AI in customer support?"
-    answer = generate_rag_response(user_input)
-    print("\nAnswer:\n", answer)
+    user_input = "I'm very frustrated, nothing is working."
+    response = generate_rag_response(user_input)
+    print(f"\nFinal Answer:\n{response}")
